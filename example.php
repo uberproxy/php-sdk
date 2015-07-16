e90bed8f3ffd0701e7458c91c18b1eba37f7e4bd
Cesar Rodas <cesar@rodas.me>
2015-07-15 22:01:33 -0400

    Add SSL-example

--- example.php
+++ example.php
@@ -5,6 +5,18 @@ require __DIR__ . "/vendor/autoload.php";
 $client = new UberProxy\Client("localhost", "8e0c5e97f91e1a8dde85702ffadff48e8488fda46c457712920aa835dabe25c8");
 assert($client->ping()['pong'] === true);
 
+$response =$client->addSsl('domain2.foobar.net')
+    ->addCert('mysitename.crt')
+    ->addKey('mysitename.key')
+    ->send();
+assert($response['error'] === false);
+
+$response = $client->register()
+    ->addClient('localhost:3333')
+    ->addHost('domain2.foobar.net')
+    ->send();
+assert($response['error'] === false);
+
 $response = $client->register()
     ->addClient('localhost:3333')
     ->addHost('domain1.foobar.net')
